a8b3a93040aadfe57b0afaadd6a6410c0beef40c
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-06-25 08:42:24 -0400

    importing questions

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const inuirer = require('inquirer');
 const fs = require('fs');
+
+//importing constructor functinos 
+const Employee = require('./lib/Employee');
+const Employee = require('./lib/Engineer');
+const Employee = require('./lib/Intern');
+const Employee = require('./lib/Manager');
+
+//importing questions
+const EngineerQuestions = require('./lib/EngineerQuestions');
+const ManagerQuestions = require('./lib/EngineerQuestions');
+const InternQuestions = require('./lib/EngineerQuestions');
+
+
+
+const questions = [
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What is the employee\'s role?',
+        choices: // function to allow only one manager to be created
+            () => {
+            if (allEmployees.some(employee => employee.role === 'Manager')) {
+                return ['Engineer', 'Intern']    
+            } else {
+                return ['Manager', 'Engineer', 'Intern']
+            }
+        }
+    },
+    {
+        
+    }
